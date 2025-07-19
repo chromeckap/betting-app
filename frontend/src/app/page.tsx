b6edac744c22d731883d8eaa902320bf33cd27dd
Petr <petr.chromecka.s01@osu.cz>
2025-07-19 18:33:00 +0200

    Added Nextjs app as frontend

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+import {IconBrandReact} from "@tabler/icons-react";
+
+export default function Home() {
+    return (
+        <div>
+            <Button size="lg">
+                <IconBrandReact/> New React App
+            </Button>
+        </div>
+    )
+}
