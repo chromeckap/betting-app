f5aa31a7f84dbe10d6ac0451f9687bd0e1b11564
Petr <petr.chromecka.s01@osu.cz>
2025-07-19 16:28:59 +0200

    Added Spring Boot app as backend

---
+++ backend/src/test/java/com/chromeckap/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chromeckap.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
