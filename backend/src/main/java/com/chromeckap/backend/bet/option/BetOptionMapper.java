480406de7095caba768dfb4c4b0eca2f05fc9772
Petr Chromečka <chromeckap@seznam.cz>
2025-09-22 20:13:12 +0200

    added logic to BetOptionService manage method
    - added the manageBetOptions() to updateBet()
    - implemented logic to manageBetOption()
    - created BetOptionMapper to convert toEntity() and updateEntityAttributes()
    - added find methods to BetOptionRepository
    - added attributes to BetOptionRequest

---
+++ backend/src/main/java/com/chromeckap/backend/bet/option/BetOptionMapper.java
@@ -0,0 +1,19 @@
+package com.chromeckap.backend.bet.option;
+
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BetOptionMapper {
+
+    public BetOption toEntity(BetOptionRequest request) {
+        return BetOption.builder()
+                .name(request.name())
+                .build();
+    }
+
+    public BetOption updateEntityAttributes(BetOption betOption, @Valid BetOptionRequest request) {
+        betOption.setName(request.name());
+        return betOption;
+    }
+}
